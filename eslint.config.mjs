591e6cd7540d4bcacd3072f5bd209c4817384de0
khk4912 <kosame@kosame.dev>
2026-05-06 23:54:30 +0900

    🔧 chore: Remove eslint tailwindcss config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,6 @@
-import { fileURLToPath } from 'node:url'
 import { defineConfig, globalIgnores } from 'eslint/config'
 import nextVitals from 'eslint-config-next/core-web-vitals'
 import nextTs from 'eslint-config-next/typescript'
-import tailwind from 'eslint-plugin-tailwindcss'
 import neostandard from 'neostandard'
 import tseslint from 'typescript-eslint'
 
@@ -13,19 +11,12 @@ const ignores = [
   'next-env.d.ts',
 ]
 
-const tailwindCssConfigPath = fileURLToPath(new URL('./app/globals.css', import.meta.url))
+// const tailwindCssConfigPath = fileURLToPath(new URL('./app/globals.css', import.meta.url))
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  tailwind.configs.recommended,
-  {
-    settings: {
-      tailwindcss: {
-        cssConfigPath: tailwindCssConfigPath
-      }
-    }
-  },
+
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // global ignores of eslint-config-next
